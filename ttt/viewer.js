867ae0ce40e7ffac64abdffb12a12dce80fc20c0
Jozef <jozefw@sbcglobal.net>
2015-01-26 21:08:20 -0800

    add list of games played to view

--- ttt/viewer.js
+++ ttt/viewer.js
@@ -3,13 +3,22 @@
 myDataRef = new Firebase('https://t-cubed.firebaseio.com/');
 
 myDataRef.on('value', function(snapshot) {
-	console.log(snapshot);
+
 	var data = snapshot.val();
 
-	console.log( data );
+	// console.log( data );
 	
-	console.log( Object.keys(data) );
-	data.sort( function(a,b) { return b.gameTime - a.gameTime; } );
-   	console.log( 'after sort');
-   	console.log( data );
+	// console.log( Object.keys(data) );
+	// data.sort( function(a,b) { return b.gameTime - a.gameTime; } );
+  var gamePicked = Object.keys(data);
+
+  var s;
+	for ( var i = 0; i < gamePicked.length; i++ ) {
+		s = '<option value="' + gamePicked[i] + '">' + gamePicked[i] + '</option>'; 
+		$("select").append(s);
+
+	}
+
+
 });
+
