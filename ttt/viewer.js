a9861dc61dcc02b0ebbaa2933b21eef19a61435c
Jozef <jozefw@sbcglobal.net>
2015-01-28 19:33:03 -0800

    append time and board to display

--- ttt/viewer.js
+++ ttt/viewer.js
@@ -4,12 +4,12 @@ var data,
 		gamePicked;
 
 myDataRef.on('value', function(snapshot) {
-	
 	data = snapshot.val();
 
 	console.log( data );
-	
-	// console.log( Object.keys(data) );
+	for ( i in data ) {
+		console.log(i, data[i].board[0]);
+	}
 	
   gamePicked = Object.keys(data);
   console.log( "this is gamePicked " + gamePicked);
@@ -21,16 +21,26 @@ myDataRef.on('value', function(snapshot) {
 		s = '<option value="' + d + '">' + d + '</option>'; 
 		$select.append(s);
 	}
-	
 });
 // grab the selected game number and display game (use jquery.. data object will have the board)
-// var m = $("#gameSelect option:selected").text();
 var m = $("#gameSelect").change(function() {
 	var str = "";
 	$("select option:selected").each(function() {
-		str = str + $(this).text() + " ";
+		str = str + $(this).text();
+			for ( i in data ) {
+				if ( str === i ) {
+				$("#choice").append(i, data[i].board);
+				}
+			};
 	})
+	// display the chosen game to screen
 	chooseGame = $("#choice").text(str);
 	
+
+// compare the choice with the data object to get the board and display to screen	
+	
+
+	
+
 });
 	
