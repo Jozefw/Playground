32d5fe7b83bcc04841bc08625816fc305977bdfa
Jozef <jozefw@sbcglobal.net>
2015-01-28 20:40:35 -0800

    link game chosen with corresponding board game

--- ttt/viewer.js
+++ ttt/viewer.js
@@ -1,6 +1,7 @@
 myDataRef = new Firebase('https://t-cubed.firebaseio.com/');
 var data,
 		chooseGame,
+		boardDisplayed,
 		gamePicked;
 
 myDataRef.on('value', function(snapshot) {
@@ -23,19 +24,23 @@ myDataRef.on('value', function(snapshot) {
 	}
 });
 // grab the selected game number and display game (use jquery.. data object will have the board)
+
 var m = $("#gameSelect").change(function() {
 	var str = "";
 	$("select option:selected").each(function() {
 		str = str + $(this).text();
-			for ( i in data ) {
-				if ( str === i ) {
-				$("#choice").append(i, data[i].board);
-				}
-			};
 	})
 	// display the chosen game to screen
 	chooseGame = $("#choice").text(str);
-	
+	for ( var j = 0; j < data.length; j++ ) {
+		for ( key in data ) {
+			if ( data.hasOwnProperty( chooseGame )){
+				console.log(data[i].board[j]);
+				$("#boardChoice").append(data[i].board[j]);
+			}
+			
+		}
+	}
 
 // compare the choice with the data object to get the board and display to screen	
 	
