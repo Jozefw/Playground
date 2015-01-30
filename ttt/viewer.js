791302ab4ec7886d49c4206f42ea3ae791cb4607
Jozef <jozefw@sbcglobal.net>
2015-01-29 22:35:09 -0800

    refactored board search

--- ttt/viewer.js
+++ ttt/viewer.js
@@ -17,7 +17,7 @@ myDataRef.on('value', function(snapshot) {
   var $select = $("select");
 	for ( var i = 0; i < gamePicked.length; i++ ) {
 		d = new Date(gamePicked[i]*1).toLocaleString();
-		s = '<option value="' + d + '">' + d + '</option>'; 
+		s = '<option value="' + ( gamePicked[i] * 1 ) + '">' + d + '</option>'; 
 		$select.append(s);
 	}
 });
@@ -26,20 +26,24 @@ myDataRef.on('value', function(snapshot) {
 var m = $("#gameSelect").change(function() {
 	var str = "";
 	$("select option:selected").each(function() {
-		str = str + $(this).text();
+		str = str + $(this).val();
 	})
-
+console.log(str);
 	// compare the choice with the data object to get the board and display to screen	
 	// for ( var j = 0; j< data.length; j++ ) {
-		for ( key in data ) {
-			if ( str === new Date(key*1).toLocaleString() ) {
-				boardDisplayed = data[key].board[0];
+		// for ( key in data ) {
+		// 	if ( str === new Date(key*1).toLocaleString() ) {
+		// 		boardDisplayed = data[key].board[0];
 				
-			}
-		}
+		// 	}
+		// }
 	// } 	
+console.log( 'board ' + data[str].board );  // all the moves in the selected game
+	chooseGame = $("#choice").text( data[str].board );
 
-	chooseGame = $("#choice").text(str + " " + boardDisplayed);
+console.log( data[str].board[0] ); // the first move
+console.log( data[str].board[1] ); // the 2nd move
+console.log( data[str].board[2] ); // the 2nd move
 
 	
  });
