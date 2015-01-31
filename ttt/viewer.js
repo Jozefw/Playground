366f9665ca856b948c2942233e7598be310da5cf
Jozef <jozefw@sbcglobal.net>
2015-01-30 22:41:48 -0800

    display all moves

--- ttt/viewer.js
+++ ttt/viewer.js
@@ -2,13 +2,13 @@ myDataRef = new Firebase('https://t-cubed.firebaseio.com/');
 var data,
 		chooseGame,
 		boardDisplayed,
+		date,
 		gamePicked;
 
 myDataRef.on('value', function(snapshot) {
 	data = snapshot.val();
 
 	console.log( data );
-	
 	
   gamePicked = Object.keys(data);
   console.log( "this are gamePicked " + gamePicked);
@@ -21,29 +21,22 @@ myDataRef.on('value', function(snapshot) {
 		$select.append(s);
 	}
 });
-// grab the selected game number and display game (use jquery.. data object will have the board)
+// grab the selected game number and display game
 
 var m = $("#gameSelect").change(function() {
 	var str = "";
 	$("select option:selected").each(function() {
+		// date = date + $(this).html();
 		str = str + $(this).val();
+
 	})
-console.log(str);
-	// compare the choice with the data object to get the board and display to screen	
-	// for ( var j = 0; j< data.length; j++ ) {
-		// for ( key in data ) {
-		// 	if ( str === new Date(key*1).toLocaleString() ) {
-		// 		boardDisplayed = data[key].board[0];
-				
-		// 	}
-		// }
-	// } 	
-console.log( 'board ' + data[str].board );  // all the moves in the selected game
-	chooseGame = $("#choice").text( data[str].board );
-
-console.log( data[str].board[0] ); // the first move
-console.log( data[str].board[1] ); // the 2nd move
-console.log( data[str].board[2] ); // the 2nd move
-
-	
+	for ( var i = 0; i <= 8; i++ ) {
+		for ( var j = 0; j <= 8; j++ ) {		
+			if ( data[str].board[i][j] === "X" || data[str].board[i][j] === "O" ) {
+			
+			var t = $( "#"+[j]).text(data[str].board[i][j] );
+			}
+		}
+	}
+		chooseGame = $("#choice").append( str );
  });
